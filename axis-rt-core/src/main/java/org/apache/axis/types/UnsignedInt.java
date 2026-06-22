c9f8d8419c9c10d64525dbfa4a0d995b997bd9dc
Pino Navato <navato@objectway.it>
2026-06-22 15:32:45 +0200

    Removed buggy implementations of the Comparable interface

--- axis-rt-core/src/main/java/org/apache/axis/types/UnsignedInt.java
+++ axis-rt-core/src/main/java/org/apache/axis/types/UnsignedInt.java
@@ -25,7 +25,7 @@ import org.apache.axis.utils.Messages;
  * @author Chris Haddad (chaddad@cobia.net)
  * @see <a href="http://www.w3.org/TR/xmlschema-2/#unsignedInt">XML Schema 3.3.22</a>
  */
-public class UnsignedInt extends java.lang.Number implements java.lang.Comparable {
+public class UnsignedInt extends java.lang.Number {
 
     protected Long lValue = new Long(0);
 
@@ -102,17 +102,6 @@ public class UnsignedInt extends java.lang.Number implements java.lang.Comparabl
               lValue.equals(other.lValue)));
         __equalsCalc = null;
         return _equals;
-    }
-
-    // implement java.lang.comparable interface
-    public int compareTo(Object obj) {
-      if (lValue != null)
-        return lValue.compareTo(obj);
-      else
-        if (equals(obj) == true)
-            return 0;  // null == null
-        else
-            return 1;  // object is greater
     }
 
     // Implement java.lang.Number interface
